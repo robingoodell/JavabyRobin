f496cb4896449c7f690094bcfc2fc7deb2144d39
robingoodell <goodellr@techhigh.us>
2014-12-30 17:44:46 -0500

    Update DrawFigures1.java

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,34 +1,26 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
+        drawCone();
+        drawBox();
+        drawLabel();
+        drawBox();
+        drawCone();
+    }
+        public static void drawCone(){
         System.out.println("   /\\");
         System.out.println("  /  \\");
         System.out.println(" /    \\");
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println();
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
+        }
+      
+        public static void drawBox(){
         System.out.println("+------+");
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
+        }
+        public static void drawLabel(){
         System.out.println("| TECH |");
         System.out.println("| HIGH |");
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-    }
-}
+        }
+        
+        
